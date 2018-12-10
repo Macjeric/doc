4173815e424b3d9f46b8a51eb194a0bf7f7ad34b
Macjeric <mack.jeric8@gmail.com>
2018-12-10 21:22:58 +0300

    ws3

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -14,42 +14,26 @@
         </div> -->
 
         <div class ="col">
-        <svg width="300" height="300"> 
+        <svg width="300" height="300"> <a href ="/themes/uni/mu">
         <rect width ="170" height ="170" style ="fill:rgb(0,50,255); stroke:rgb(0,0,0)" />
-        <text x="50" y="100" font-family="sans-serif" font-size="40px" fill="black">MU</text>
+        <text x="50" y="100" font-family="sans-serif" font-size="40px" fill="black">MU</text></a>
         </svg>
         </div>
 
         <div class ="col">
-        <svg width="300" height="300"> 
+        <svg width="300" height="300"> <a href ="/themes/uni/iaa">
         <rect width ="170" height ="170" style ="fill:rgb(0,50,255); stroke:rgb(0,0,0)" />
-        <text x="50" y="100" font-family="sans-serif" font-size="40px" fill="black">IAA</text>
+        <text x="50" y="100" font-family="sans-serif" font-size="40px" fill="black">IAA</text></a>
         </svg>
         </div>
 
-        <!-- <div class="col">
-        <a href ="/themes/uni/sua"> <img src="/images/sua.jpg"</img> </a>
-            
-        </div> -->
-
-        <!-- <div class="col">
-        <a href ="/themes/uni/udsm"> <img src="/images/udsm.jpg"</img> </a>  
-            
-        </div> -->
 
         
 </div>
     <div class="row">
 
 
-        <!-- <div class="col">
-        <a href ="/themes/uni/iaa"> <img src="/images/iaa.jpg"</img>    </a>           
-        </div> -->
-
-
-            <!-- <div class="col">
-        <a href ="/themes/uni/ifm"> <img src="/images/ifm.jpg"</img>        </a>       
-        </div> -->
+      
 
 
 
